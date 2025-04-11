21b546a28363e355b456a94ceb5c31cc8ddffd21
PJ-JP <a20213805@pucp.edu.pe>
2025-04-10 21:48:13 -0500

    feat:jugador

--- src/main/java/com/example/laboratorio/entity/Jugador.java
+++ src/main/java/com/example/laboratorio/entity/Jugador.java
@@ -1,7 +1,6 @@
 package com.example.laboratorio.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,5 +9,25 @@ import lombok.Setter;
 @Entity
 @Table(name = "jugador")
 public class Jugador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idjugador", nullable = false)
+    private Integer idjugador;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "edad", nullable = false)
+    private Integer edad;
+
+    @Column(name = "posicion", nullable = false)
+    private String posicion;
+
+    @Column(name = "club", nullable = false)
+    private String club;
+
+    @Column(name = "seleccion", nullable = false)
+    private Integer seleccion;
+
 
 }
