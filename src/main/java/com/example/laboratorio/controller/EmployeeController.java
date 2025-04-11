4ac2fe95e37910619e0ef8d044f8c11dae219904
PJ-JP <a20213805@pucp.edu.pe>
2025-04-10 19:58:49 -0500

    All

---
+++ src/main/java/com/example/laboratorio/controller/EmployeeController.java
@@ -0,0 +1,87 @@
+package com.example.laboratorio.controller;
+
+
+import com.example.laboratorio.entity.Employee;
+import com.example.laboratorio.repository.EmployeeRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/shipper")
+public class EmployeeController {
+
+    final EmployeeRepository shipperRepository;
+
+    public EmployeeController(EmployeeRepository shipperRepository) {
+        this.shipperRepository = shipperRepository;
+    }
+
+    @GetMapping(value = {"/list", ""})
+    public String listarTransportistas(Model model) {
+
+        List<Employee> lista = shipperRepository.findAll();
+        model.addAttribute("shipperList", lista);
+
+        return "shipper/list";
+    }
+
+    @GetMapping("/new")
+    public String nuevoTransportistaFrm() {
+        return "shipper/newFrm";
+    }
+
+    @PostMapping("/save")
+    public String guardarNuevoTransportista(Employee shipper, RedirectAttributes attr) {
+        shipperRepository.save(shipper);
+        return "redirect:/shipper/list";
+    }
+
+    @GetMapping("/edit")
+    public String editarTransportista(Model model,
+                                      @RequestParam("id") int id) {
+
+        Optional<Employee> optShipper = shipperRepository.findById(id);
+
+        if (optShipper.isPresent()) {
+            Employee shipper = optShipper.get();
+            model.addAttribute("shipper", shipper);
+            return "shipper/editFrm";
+        } else {
+            return "redirect:/shipper/list";
+        }
+    }
+
+    @GetMapping("/delete")
+    public String borrarTransportista(Model model,
+                                      @RequestParam("id") int id,
+                                      RedirectAttributes attr) {
+
+        Optional<Employee> optShipper = shipperRepository.findById(id);
+
+        if (optShipper.isPresent()) {
+            shipperRepository.deleteById(id);
+        }
+        return "redirect:/shipper/list";
+
+    }
+
+    @PostMapping("/BuscarTransportistas")
+    public String buscarTransportista(@RequestParam("searchField") String searchField,
+                                      Model model) {
+
+        List<Employee> listaTransportistas = shipperRepository.buscarTransPorCompName(searchField);
+        model.addAttribute("shipperList", listaTransportistas);
+
+        return "shipper/list";
+    }
+
+
+}
